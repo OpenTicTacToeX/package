09eb61124eb5937d45c16666d26899a980654a9d
Manuel <manuel@debian-BULLSEYE-live-builder-AMD64>
2021-09-03 21:26:38 -0400

    fix debs

--- createdebs.sh
+++ createdebs.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "Version?"
+echo
 read version
 
 cd deb64
-dpkg -b . ../export/opentictactoe_$version-amd64.deb
+dpkg -b . ../export/opentictactoex_$version-amd64.deb
 cd ..
 cd deb32
-dpkg -b . ../export/opentictactoe_$version-i386.deb
+dpkg -b . ../export/opentictactoex_$version-i386.deb
 
+echo "Exported!"
